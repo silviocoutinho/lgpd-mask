cb94ab7e02297a5bb1cd68dafc2588734f23501f
Nilson-Sj <nilsonjr180795@gmail.com>
2023-09-27 11:07:05 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,69 @@
+# Mascarar LGPD - Aplicativo para Remover Dados Sensíveis de PDFs
+
+Este é um aplicativo simples desenvolvido em Python usando a biblioteca Tkinter e PyMuPDF (PyMuPDF é uma biblioteca para trabalhar com arquivos PDF) que permite mascarar informações sensíveis, como CPF e RG, em documentos PDF. O aplicativo oferece uma interface gráfica amigável para facilitar a remoção desses dados sensíveis de arquivos PDF de forma eficiente.
+
+## Funcionalidades
+
+O projeto oferece as seguintes funcionalidades:
+
+- Inserção de números de CPF e RG em campos de entrada de texto.
+- Seleção de um arquivo PDF no sistema de arquivos.
+- Escolha de uma pasta de destino para salvar o arquivo PDF mascarado.
+- Máscara para formatar e ocultar números de CPF e RG.
+- Limpeza dos campos preenchidos.
+- Interface gráfica intuitiva e de fácil utilização.
+
+## Como Usar
+
+Para usar o aplicativo, siga estas etapas:
+
+1. **Requisitos**: Você precisará ter o Python instalado em seu sistema. Este aplicativo foi desenvolvido com o Python 3.
+
+2. **Bibliotecas**: Instale as bibliotecas `tkinter` e `PyMuPDF` se elas não estiverem instaladas no seu sistema. Você pode instalá-lo usando o seguinte comando:
+
+    pip install tkinter
+
+    pip install PyMuPDF    
+
+3. **Baixe o Código-Fonte**: Clone este repositório para sua máquina local:
+
+    git clone [URL do Repositório]
+
+4. **Execute o Aplicativo**:
+   - Abra um terminal ou prompt de comando.
+   - Navegue até a pasta onde você baixou o código deste aplicativo:
+
+    cd #projeto...#
+
+    - Execute o aplicativo com o seguinte comando:
+     ```
+     python main.py
+
+     ou
+     ```
+    - Execute o arquivo Python `main.py` para iniciar a aplicação.
+
+5. **Interface Gráfica**:
+   - Na interface gráfica, você verá os seguintes campos e botões:
+
+     - **CPF e RG**: Insira os números de CPF e RG nos campos de entrada apropriados.
+     - **Arquivo PDF**: Clique no campo de entrada e selecione um arquivo PDF para processar.
+     - **Pasta de Destino**: Clique no campo de entrada e selecione a pasta de destino onde o PDF mascarado será salvo.
+     - **Mascarar**: Clique neste botão para aplicar a máscara aos números de CPF e RG inseridos.
+     - **Limpar**: Clique neste botão para limpar todos os campos e recomeçar.
+
+6. **Mascarar Informações**: Após inserir CPF, RG e selecionar um arquivo PDF e uma pasta de destino, clique no botão "Mascarar" para aplicar a máscara aos números de CPF e RG. O PDF mascarado será salvo na pasta de destino selecionada.
+
+7. **Limpar Campos**: Se desejar, você pode limpar todos os campos preenchidos clicando no botão "Limpar".
+
+## Personalização
+
+Você pode personalizar este aplicativo de acordo com suas necessidades, incluindo a implementação das funções de máscara para CPF e RG.
+
+## Contribuições
+
+Contribuições são bem-vindas! Sinta-se à vontade para melhorar este aplicativo e enviar solicitações de pull com novos recursos ou correções de bugs.
+
+-----------------------------------------------------------
+
+Feito por 🚀 NN 🚀
---
+++ main.py
@@ -0,0 +1,203 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox
+import fitz
+import os.path
+import re
+
+# Variáveis globais para armazenar o caminho do arquivo PDF e a pasta de destino
+caminhoArquivo = ""
+destino = ""
+
+# Variáveis globais para controlar se CPF e RG estão habilitados
+cpf_enabled = True
+rg_enabled = True
+
+# Função para alternar a habilitação do campo CPF
+def toggle_cpf():
+    global cpf_enabled
+    cpf_enabled = not cpf_enabled
+    cpf_entry.config(state=tk.NORMAL if cpf_enabled else tk.DISABLED)
+
+# Função para alternar a habilitação do campo RG
+def toggle_rg():
+    global rg_enabled
+    rg_enabled = not rg_enabled
+    rg_entry.config(state=tk.NORMAL if rg_enabled else tk.DISABLED)
+
+# Função para limpar os campos de entrada
+def clear_button():
+    cpf_entry.delete(0, tk.END)
+    rg_entry.delete(0, tk.END)
+    pdf_file_entry.delete(0, tk.END)
+    dest_folder_entry.delete(0, tk.END)
+
+# Função para limpar as siglas do RG
+def clean_rg(rg_pdf, user_input_rg):
+    # Remove caracteres indesejados da entrada do usuário
+    user_input_rg = re.sub(r'[^0-9a-zA-Z\s/.-]', '', user_input_rg)
+    cleaned_rg = ""
+    rg_parts = rg_pdf.split()
+    user_input_parts = user_input_rg.split()
+
+    # Itera sobre as partes do RG do PDF e compara com a entrada do usuário
+    for part in rg_parts:
+        if any(part.startswith(prefix) for prefix in user_input_parts):
+            cleaned_rg += part + " "
+        elif re.sub(r'\D', '', part) in user_input_parts:
+            cleaned_rg += part + " "
+
+    return cleaned_rg.strip()
+
+# Função para realizar a máscara de CPF e RG
+def mask():
+    global cpf_enabled, rg_enabled
+    cpf = cpf_entry.get()
+    rg = rg_entry.get()
+
+    if not cpf_enabled and not rg_enabled:
+        messagebox.showerror("Erro", "Por favor, selecione pelo menos CPF ou RG.")
+        return  # Não continue se ambos estiverem desativados
+    elif not os.path.isfile(caminhoArquivo):
+        messagebox.showerror("Erro", "O arquivo selecionado não existe.")
+        return  # Não continue se o arquivo não existir
+
+    arquivo = caminhoArquivo
+    doc = fitz.open(arquivo)
+    achouCPF = False
+    achouRG = False
+    mensagem = []
+
+    cpf_digitos = re.sub(r'\D', '', cpf)
+
+    if cpf_enabled and cpf:
+        cpf_pattern = re.compile(r'[\d\s.-]+')
+        for page in doc:
+            text = page.get_text()
+            cpf_matches = cpf_pattern.findall(text)
+
+            for match in cpf_matches:
+                cpf_pdf = re.sub(r'\D', '', match)
+                if cpf_pdf == cpf_digitos:
+                    rect = page.search_for(match)
+                    for r in rect:
+                        a = page.add_redact_annot(r, fill=(0, 0, 0))
+                        a.update()
+                        page.apply_redactions()
+                    achouCPF = True
+                    mensagem.append("CPF removido com sucesso.")
+                    cpf_entry.delete(0, tk.END)
+
+    rg_digitos = re.sub(r'[^0-9a-zA-Z\s/.-]', '', rg)
+
+    if rg_enabled and rg:
+        rg_pattern = re.compile(r'[0-9a-zA-Z\s/.-]+')
+        for page in doc:
+            text = page.get_text()
+            rg_matches = rg_pattern.findall(text)
+
+            for match in rg_matches:
+                rg_pdf = re.sub(r'[^0-9a-zA-Z\s/.-]', '', match)
+                cleaned_rg = clean_rg(rg_pdf, rg)
+                
+                if cleaned_rg == rg_digitos:
+                    rect = page.search_for(match)
+                    for r in rect:
+                        a = page.add_redact_annot(r, fill=(0, 0, 0))
+                        a.update()
+                        page.apply_redactions()
+                    achouRG = True
+                    mensagem.append("RG removido com sucesso.")
+                    rg_entry.delete(0, tk.END)
+
+    if mensagem:
+        mensagem_final = []
+        if achouCPF:
+            mensagem_final.append("CPF removido com sucesso.")
+        if achouRG:
+            mensagem_final.append("RG removido com sucesso.")
+        
+        nome_arquivo_destino = os.path.basename(caminhoArquivo)
+        caminho_destino = os.path.join(destino, nome_arquivo_destino)
+        doc.save(caminho_destino)
+        
+        messagebox.showinfo("Sucesso", "\n".join(mensagem_final))
+    else:
+        messagebox.showinfo("Informação", "Nenhum dado encontrado para remoção.")
+
+# Função para selecionar um arquivo PDF
+def select_pdf_file():
+    global caminhoArquivo
+    pdf_file_path = filedialog.askopenfilename(filetypes=[("PDF Files", "*.pdf")])
+    pdf_file_entry.delete(0, tk.END)
+    pdf_file_entry.insert(0, pdf_file_path)
+    caminhoArquivo = pdf_file_path
+
+# Função para selecionar uma pasta de destino
+def select_destination_folder():
+    global destino
+    dest_folder_path = filedialog.askdirectory()
+    dest_folder_entry.delete(0, tk.END)
+    dest_folder_entry.insert(0, dest_folder_path)
+    destino = dest_folder_path
+
+# Criação da interface gráfica usando Tkinter
+root = tk.Tk()
+root.title("Mascarar LGPD")
+root.geometry("650x250")
+
+# Rótulo do título
+title_label = tk.Label(root, text="Mascarar LGPD")
+title_label.grid(row=0, column=0, columnspan=4, pady=(10, 5))
+
+# Rótulo e campo de entrada para CPF
+cpf_label = tk.Label(root, text="CPF:")
+cpf_label.grid(row=1, column=0, sticky="w", padx=10, pady=5)
+cpf_entry = tk.Entry(root, width=40)
+cpf_entry.grid(row=1, column=1, columnspan=2, sticky="ew", padx=10, pady=5)
+
+# Checkbox para habilitar/desabilitar CPF
+cpf_checkbox = tk.Checkbutton(root, text="Ativo", command=toggle_cpf)
+cpf_checkbox.grid(row=1, column=3, padx=10, pady=5)
+cpf_checkbox.select()
+
+# Rótulo e campo de entrada para RG
+rg_label = tk.Label(root, text="RG:")
+rg_label.grid(row=2, column=0, sticky="w", padx=10, pady=5)
+rg_entry = tk.Entry(root, width=40)
+rg_entry.grid(row=2, column=1, columnspan=2, sticky="ew", padx=10, pady=5)
+
+# Checkbox para habilitar/desabilitar RG
+rg_checkbox = tk.Checkbutton(root, text="Ativo", command=toggle_rg)
+rg_checkbox.grid(row=2, column=3, padx=10, pady=5)
+rg_checkbox.select()
+
+# Rótulo e campo de entrada para o arquivo PDF
+pdf_file_label = tk.Label(root, text="Arquivo PDF:")
+pdf_file_label.grid(row=3, column=0, sticky="w", padx=10, pady=5)
+pdf_file_entry = tk.Entry(root, width=40)
+pdf_file_entry.grid(row=3, column=1, columnspan=2, sticky="ew", padx=10, pady=5)
+
+# Botão para selecionar um arquivo PDF
+pdf_file_button = tk.Button(root, text="Selecionar", command=select_pdf_file)
+pdf_file_button.grid(row=3, column=3, pady=5)
+
+# Rótulo e campo de entrada para a pasta de destino
+dest_folder_label = tk.Label(root, text="Pasta de destino:")
+dest_folder_label.grid(row=4, column=0, sticky="w", padx=10, pady=5)
+dest_folder_entry = tk.Entry(root, width=40)
+dest_folder_entry.grid(row=4, column=1, columnspan=2, sticky="ew", padx=10, pady=5)
+
+# Botão para selecionar uma pasta de destino
+dest_folder_button = tk.Button(root, text="Selecionar", command=select_destination_folder)
+dest_folder_button.grid(row=4, column=3, pady=5)
+
+# Botão para mascarar os dados
+mask_button = tk.Button(root, text="Mascarar", command=mask, width=10)
+mask_button.grid(row=5, column=2, sticky="e", padx=10, pady=5)
+
+# Botão para limpar os campos de entrada
+clear_button = tk.Button(root, text="Limpar", command=clear_button, width=10)
+clear_button.grid(row=5, column=3, sticky="e", pady=5)
+
+# Iniciar a interface gráfica
+root.mainloop()
