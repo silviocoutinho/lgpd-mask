e3960d6df2e0b080d594ca244a76f9f61fbf69f8
Nilson Junior <108434705+Nilson-Sj@users.noreply.github.com>
2023-10-04 15:44:27 -0300

    Refatoracao: Separacao de funcoes em modulos

--- mascararLGPD/pdf_mascarar.py
+++ mascararLGPD/pdf_mascarar.py
@@ -1,38 +1,8 @@
 import fitz
 import os
 import re
-
-def _remover_cpf(pagina, cpf_limpo):
-    padrao_cpf = re.compile(r'CPF\s*n[°ºº.]*\s*([\d]{3}[-.\s]?[\d]{3}[-.\s]?[\d]{3}[-.\s]?\s?[\d]{2})')
-    cpf_encontrados = padrao_cpf.findall(pagina.get_text())
-
-    for encontrado in cpf_encontrados:
-        cpf_pdf = re.sub(r'\D', '', encontrado)
-        if cpf_pdf == cpf_limpo:
-            retangulos = pagina.search_for(encontrado)
-            for retangulo in retangulos:
-                anotacao = pagina.add_redact_annot(retangulo, fill=(0, 0, 0))
-                anotacao.update()
-                pagina.apply_redactions()
-            return True
-
-    return False
-
-def _remover_rg(pagina, rg_limpo):
-    padrao_rg = re.compile(r'RG\s*n[°ºº.]*\s*((?:[A-Z]+\s*)?[-.\s]?\s*[\d.-]+(?:\s*[–-]\s*[A-Z/]+[A-Z/]+)?)')
-    rg_encontrados = padrao_rg.findall(pagina.get_text())
-
-    for encontrado in rg_encontrados:
-        rg_pdf = re.sub(r'\D', '', encontrado)
-        if rg_pdf == rg_limpo:
-            retangulos = pagina.search_for(encontrado)
-            for retangulo in retangulos:
-                anotacao = pagina.add_redact_annot(retangulo, fill=(0, 0, 0))
-                anotacao.update()
-                pagina.apply_redactions()
-            return True
-
-    return False
+from .cpf_mascarar import _remover_cpf
+from .rg_mascarar import _remover_rg
 
 def mascarar(caminho_arquivo, destino, cpf_ativo, rg_ativo, cpf, rg):
     doc = fitz.open(caminho_arquivo)
