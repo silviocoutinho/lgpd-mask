573979fff56c810049e4e859a32f3fb8b60c473f
Nilson-Sj <nilsonjr180795@gmail.com>
2023-10-10 10:53:58 -0300

    Refatoracao: Adicionei comentários explicativos

--- mascararDados/mascararPdf.py
+++ mascararDados/mascararPdf.py
@@ -5,24 +5,35 @@ import re
 from .mascararCpf import removerCpf
 from .mascararRg import removerRg
 
+# Função principal para mascarar dados
 def mascarar(caminhoArquivo, destino, cpfAtivo, rgAtivo, cpf, rg, entradaCpf, entradaRg):
+
+# Abre o arquivo PDF usando a biblioteca PyMuPDF (fitz)
     doc = fitz.open(caminhoArquivo)
+
+# Inicializa variáveis de controle e mensagens    
     achouCpf = False
     achouRg = False
     mensagem = []
 
+ # Limpa os caracteres não numéricos dos valores de CPF e RG
     cpfLimpo = re.sub(r'\D', '', cpf)
     rgLimpo = re.sub(r'\D', '', rg)
 
+# Itera sobre cada página do documento PDF
     for pagina in doc:
+
+# Verifica se a opção de CPF está ativa e tenta remover o CPF da página
         if cpfAtivo and removerCpf(pagina, cpfLimpo):
             achouCpf = True
-            mensagem.append("CPF encontrado com sucesso.")
+            mensagem.append("CPF encontrado com sucesso.")   # Exibe mensagens de resultado
 
+# Verifica se a opção de RG está ativa e tenta remover o RG da página
         if rgAtivo and removerRg(pagina, rgLimpo):
             achouRg = True
-            mensagem.append("RG encontrado com sucesso.")
+            mensagem.append("RG encontrado com sucesso.")    # Exibe mensagens de resultado
 
+# Limpa os campos de entrada de CPF e RG, se os dados foram encontrados e removidos
     if achouCpf:
         entradaCpf.delete(0, tk.END)
     else:
@@ -33,6 +44,7 @@ def mascarar(caminhoArquivo, destino, cpfAtivo, rgAtivo, cpf, rg, entradaCpf, en
     else:
         mensagem.append("RG não foi encontrado.")    
 
+# Se pelo menos um dos dados foi encontrado e removido, salva o PDF mascarado no destino
     if achouCpf or achouRg:
         arquivoDestino = os.path.basename(caminhoArquivo)
         caminhoDestino = os.path.join(destino, arquivoDestino)
