797d3e15f8b85122b457d877e887fe57c9e54742
Nilson-Sj <nilsonjr180795@gmail.com>
2023-10-10 10:57:24 -0300

    Refatoracao: Adicionei comentários explicativos

--- mascararDados/mascararCpf.py
+++ mascararDados/mascararCpf.py
@@ -1,17 +1,30 @@
 import re
 
+# Função para remover CPF de uma página PDF
 def removerCpf(pagina, cpfLimpo):
+# Define um padrão de regex para encontrar CPF no texto da página    
     padraoCpf = re.compile(r'CPF\s*n[°ºº.]*\s*([\d]{3}[-.\s]?[\d]{3}[-.\s]?[\d]{3}[-.\s]?\s?[\d]{2})')
+
+# Procura por todas as ocorrências do padrão no texto da página
     cpfEncontrados = padraoCpf.findall(pagina.get_text())
 
+ # Itera sobre os CPFs encontrados na página
     for encontrado in cpfEncontrados:
+
+# Remove caracteres não numéricos do CPF encontrado        
         cpfPdf = re.sub(r'\D', '', encontrado)
+
+# Verifica se o CPF encontrado corresponde ao CPF que deve ser removido
         if cpfPdf == cpfLimpo:
+
+# Encontra os retângulos que correspondem ao CPF no documento PDF
             retangulos = pagina.search_for(encontrado)
+
+# Itera sobre os retângulos e adiciona anotações de redação (blackout) sobre o CPF
             for retangulo in retangulos:
                 anotacao = pagina.add_redact_annot(retangulo, fill=(0, 0, 0))
                 anotacao.update()
                 pagina.apply_redactions()
-            return True
+            return True # Retorna True se um CPF for encontrado e removido
 
-    return False
+    return False    # Retorna False se nenhum CPF for encontrado e removido
