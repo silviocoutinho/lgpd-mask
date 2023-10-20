aa1ae5b3556d97dc380edb35746a0e7b4847229f
Nilson-Sj <nilsonjr180795@gmail.com>
2023-10-20 15:57:50 -0300

    Correcao: Remocao de comentarios desnecessarios

--- mascararDados/mascararPdf.py
+++ mascararDados/mascararPdf.py
@@ -5,13 +5,10 @@ import re
 from .mascararCpf import removerCpf
 from .mascararRg import removerRg
 
-# Função principal para mascarar dados
 def mascarar(caminhoArquivo, destino, cpfAtivo, rgAtivo, cpf, rg, entradaCpf, entradaRg):
 
-# Abre o arquivo PDF usando a biblioteca PyMuPDF (fitz)
     doc = fitz.open(caminhoArquivo)
 
-# Inicializa variáveis de controle e mensagens    
     achouCpf = False
     achouRg = False
     mensagem = []
@@ -20,20 +17,16 @@ def mascarar(caminhoArquivo, destino, cpfAtivo, rgAtivo, cpf, rg, entradaCpf, en
     cpfLimpo = re.sub(r'\D', '', cpf)
     rgLimpo = re.sub(r'\D', '', rg)
 
-# Itera sobre cada página do documento PDF
     for pagina in doc:
 
-# Verifica se a opção de CPF está ativa e tenta remover o CPF da página
         if cpfAtivo and removerCpf(pagina, cpfLimpo):
             achouCpf = True
-            mensagem.append("CPF encontrado com sucesso.")   # Exibe mensagens de resultado
+            mensagem.append("CPF encontrado com sucesso.")   
 
-# Verifica se a opção de RG está ativa e tenta remover o RG da página
         if rgAtivo and removerRg(pagina, rgLimpo):
             achouRg = True
-            mensagem.append("RG encontrado com sucesso.")    # Exibe mensagens de resultado
+            mensagem.append("RG encontrado com sucesso.")    
 
-# Limpa os campos de entrada de CPF e RG, se os dados foram encontrados e removidos
     if achouCpf:
         entradaCpf.delete(0, tk.END)
     else:
@@ -44,7 +37,6 @@ def mascarar(caminhoArquivo, destino, cpfAtivo, rgAtivo, cpf, rg, entradaCpf, en
     else:
         mensagem.append("RG não foi encontrado.")    
 
-# Se pelo menos um dos dados foi encontrado e removido, salva o PDF mascarado no destino
     if achouCpf or achouRg:
         arquivoDestino = os.path.basename(caminhoArquivo)
         caminhoDestino = os.path.join(destino, arquivoDestino)
