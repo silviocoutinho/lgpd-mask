1ffcd490f85ec2dce6d1b74463da69dc61e31bd4
Nilson-Sj <nilsonjr180795@gmail.com>
2023-10-20 15:56:21 -0300

    Correcao: Remocao de comentarios desnecessarios

--- desfazerRefazer.py
+++ desfazerRefazer.py
@@ -4,21 +4,17 @@ class desfazerRefazer:
         self.entrada = entrada
         self.texto = tk.StringVar()
         self.entrada.config(textvariable=self.texto)
-        self.historico = [] # Lista para armazenar o histórico de estados do texto
-        self.indice = -1    # Índice atual no histórico
+        self.historico = [] 
+        self.indice = -1   
 
-# Adiciona um rastreador para detectar mudanças no texto da entrada
         self.texto.trace_add("write", self.adicionarEstado)
 
-# Liga a função 'desfazer' à combinação de teclas 'Control-Z'
         self.entrada.bind("<Control-z>", self.desfazer)
         
-# Liga a função 'refazer' à combinação de teclas 'Control-Y'        
         self.entrada.bind("<Control-y>", self.refazer)
 
     def adicionarEstado(self, *args):
 
-# Esta função é chamada sempre que o texto da entrada é modificado        
         estadoAtual = self.texto.get()
         if self.historico and estadoAtual == self.historico[self.indice]:
             return  # Se o estado atual for igual ao último, não adiciona ao histórico
@@ -28,7 +24,6 @@ class desfazerRefazer:
 
     def desfazer(self, evento):
 
-# Esta função é chamada quando a combinação de teclas 'Control-Z' é pressionada       
         if self.indice > 0:     # Verifica se há estados anteriores no histórico
             self.indice -= 1    # Move para o estado anterior
             self.texto.set(self.historico[self.indice])     # Define o texto como o estado anterior
@@ -36,7 +31,6 @@ class desfazerRefazer:
 
     def refazer(self, evento):
 
-# Esta função é chamada quando a combinação de teclas 'Control-Y' é pressionada        
         if self.indice < len(self.historico) - 1:   # Verifica se há estados posteriores no histórico
             self.indice += 1    # Move para o estado posterior
             self.texto.set(self.historico[self.indice])     # Define o texto como o estado posterior
