620902239bcbc1cc55ce7cb566ac22fbd6ed6f63
Nilson-Sj <nilsonjr180795@gmail.com>
2023-10-26 11:31:42 -0300

    Correcao: Adicionei tratamento de excecoes no processamento de arquivos pdf

--- mascararDados/mascararPdf.py
+++ mascararDados/mascararPdf.py
@@ -7,40 +7,45 @@ from .mascararRg import removerRg
 
 def mascarar(caminhoArquivo, destino, cpfAtivo, rgAtivo, cpf, rg, entradaCpf, entradaRg):
 
-    doc = fitz.open(caminhoArquivo)
+    try:
+        doc = fitz.open(caminhoArquivo)
 
-    achouCpf = False
-    achouRg = False
-    mensagem = []
+        achouCpf = False
+        achouRg = False
+        mensagem = []
 
- # Limpa os caracteres não numéricos dos valores de CPF e RG
-    cpfLimpo = re.sub(r'\D', '', cpf)
-    rgLimpo = re.sub(r'\D', '', rg)
+    # Limpa os caracteres não numéricos dos valores de CPF e RG
+        cpfLimpo = re.sub(r'\D', '', cpf)
+        rgLimpo = re.sub(r'\D', '', rg)
 
-    for pagina in doc:
+        for pagina in doc:
 
-        if cpfAtivo and removerCpf(pagina, cpfLimpo):
-            achouCpf = True
-            mensagem.append("CPF encontrado com sucesso.")   
+            if cpfAtivo and removerCpf(pagina, cpfLimpo):
+                achouCpf = True
+                mensagem.append("CPF encontrado com sucesso.")   
 
-        if rgAtivo and removerRg(pagina, rgLimpo):
-            achouRg = True
-            mensagem.append("RG encontrado com sucesso.")    
+            if rgAtivo and removerRg(pagina, rgLimpo):
+                achouRg = True
+                mensagem.append("RG encontrado com sucesso.")    
 
-    if achouCpf:
-        entradaCpf.delete(0, tk.END)
-    else:
-        mensagem.append("CPF não foi encontrado.")
+        if achouCpf:
+            entradaCpf.delete(0, tk.END)
+        else:
+            mensagem.append("CPF não foi encontrado.")
 
-    if achouRg:
-        entradaRg.delete(0, tk.END)
-    else:
-        mensagem.append("RG não foi encontrado.")    
+        if achouRg:
+            entradaRg.delete(0, tk.END)
+        else:
+            mensagem.append("RG não foi encontrado.")    
 
-    if achouCpf or achouRg:
-        arquivoDestino = os.path.basename(caminhoArquivo)
-        caminhoDestino = os.path.join(destino, arquivoDestino)
-        doc.save(caminhoDestino)
-        return "\n".join(mensagem)
-    else:
-        return "\n".join(mensagem)
+        if achouCpf or achouRg:
+            arquivoDestino = os.path.basename(caminhoArquivo)
+            caminhoDestino = os.path.join(destino, arquivoDestino)
+            doc.save(caminhoDestino)
+            return "\n".join(mensagem)
+        else:
+            return "\n".join(mensagem)
+    
+    except Exception as e:
+        print(f"Erro ao processar o arquivo PDF: {e}")
+        return "Ocorreu um erro ao processar o arquivo PDF."
