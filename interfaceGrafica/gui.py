4db757e75925cd6cb855f73185b5a97fc89085dc
Nilson-Sj <nilsonjr180795@gmail.com>
2023-10-27 09:45:35 -0300

    Refatoracao: Simplifiquei a função limparCampos usando lambda

--- interfaceGrafica/gui.py
+++ interfaceGrafica/gui.py
@@ -5,7 +5,7 @@ from .selecionarArquivoPdf import selecionarArquivoPdf
 from .selecionarPastaDestino import selecionarPastaDestino
 from .funcoesAuxiliares.ativarCpf import ativarCpf
 from .funcoesAuxiliares.ativarRg import ativarRg
-from .limparCampos import limparCampos
+from .funcoesAuxiliares.limparCampos import limparCampos
 
 def create_gui(app):
 
@@ -66,7 +66,7 @@ def create_gui(app):
     botaoMascarar = tk.Button(app, text="Mascarar", command=lambda:mascarar(app, app.caminhoArquivoPdf, app.pastaDestino, app.cpfAtivo, app.rgAtivo), width=10)
     botaoMascarar.grid(row=5, column=2, sticky="e", padx=10, pady=5)
 
-    botaoLimpar = tk.Button(app, text="Limpar", command=botaoLimparCampos, width=10)
+    botaoLimpar = tk.Button(app, text="Limpar", command=lambda:limparCampos(entradaCpf, entradaRg, entradaArquivoPdf, entradaPastaDestino), width=10)
     botaoLimpar.grid(row=5, column=3, sticky="e", pady=5)
 
     app.mainloop()
