1ae4f1165063114daee2757ed49e7be1d36b14d0
Nilson-Sj <nilsonjr180795@gmail.com>
2023-10-27 12:20:56 -0300

    Refatoracao: removi a funcao desfazerRefazer

--- interfaceGrafica/componentes/campoTexto.py
+++ interfaceGrafica/componentes/campoTexto.py
@@ -1,8 +1,6 @@
 import tkinter as tk
-from ..funcoesAuxiliares.desfazerRefazer import desfazerRefazer
 
 def criarCampoTexto(app, width, row, column):
     campoTexto = tk.Entry(app, width=width)
     campoTexto.grid(row=row, column=column, sticky='ew')
-    campoTexto = desfazerRefazer(campoTexto)
     return campoTexto
