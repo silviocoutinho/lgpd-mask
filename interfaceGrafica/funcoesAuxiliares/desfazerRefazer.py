b165dc00b46779d934247aa18e9b34d499923b69
Nilson-Sj <nilsonjr180795@gmail.com>
2023-11-01 11:05:16 -0300

    Refatoracao: Melhorias na classe desfazerRefazer

--- interfaceGrafica/funcoesAuxiliares/desfazerRefazer.py
+++ interfaceGrafica/funcoesAuxiliares/desfazerRefazer.py
@@ -1,37 +1,40 @@
 import tkinter as tk
+
 class desfazerRefazer:
     def __init__(self, entrada):
         self.entrada = entrada
         self.texto = tk.StringVar()
         self.entrada.config(textvariable=self.texto)
-        self.historico = [] 
-        self.indice = -1   
+        self.historico = []
+        self.indice = -1
 
         self.texto.trace_add("write", self.adicionarEstado)
 
-        self.entrada.bind("<Control-z>", self.desfazer)
-        
-        self.entrada.bind("<Control-y>", self.refazer)
+        # Mapeia os eventos de teclado para funções
+        entrada.bind("<Control-z>", self.desfazer)
+        entrada.bind("<Control-y>", self.refazer)
 
     def adicionarEstado(self, *args):
-
         estadoAtual = self.texto.get()
         if self.historico and estadoAtual == self.historico[self.indice]:
-            return  # Se o estado atual for igual ao último, não adiciona ao histórico
-        self.historico = self.historico[:self.indice + 1]   # Remove estados após o índice atual
-        self.historico.append(estadoAtual)  # Adiciona o estado atual ao histórico
-        self.indice += 1    # Incrementa o índice para refletir o estado atual
+            return
+
+        # Remove estados após o índice atual e adiciona o estado atual ao histórico
+        self.historico = self.historico[:self.indice + 1]
+        self.historico.append(estadoAtual)
+        self.indice += 1
 
     def desfazer(self, evento):
-
-        if self.indice > 0:     # Verifica se há estados anteriores no histórico
-            self.indice -= 1    # Move para o estado anterior
-            self.texto.set(self.historico[self.indice])     # Define o texto como o estado anterior
-            self.entrada.icursor(tk.END)    # Define o cursor no final do texto
+        if self.indice > 0:
+            self.indice -= 1
+            self.atualizarTexto()
 
     def refazer(self, evento):
+        if self.indice < len(self.historico) - 1:
+            self.indice += 1
+            self.atualizarTexto()
 
-        if self.indice < len(self.historico) - 1:   # Verifica se há estados posteriores no histórico
-            self.indice += 1    # Move para o estado posterior
-            self.texto.set(self.historico[self.indice])     # Define o texto como o estado posterior
-            self.entrada.icursor(tk.END)    # Define o cursor no final do texto
+    def atualizarTexto(self):
+        # Atualiza o texto da entrada e coloca o cursor no final
+        self.texto.set(self.historico[self.indice])
+        self.entrada.icursor(tk.END)
