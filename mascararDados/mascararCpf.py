9bf5c67423423065b95f18b31f7a139516f0c76a
Nilson-Sj <nilsonjr180795@gmail.com>
2023-11-07 09:34:37 -0300

    Refatoracao: Adicionado tratamento de erros em mascararCpf

--- mascararDados/mascararCpf.py
+++ mascararDados/mascararCpf.py
@@ -1,25 +1,28 @@
 import re
+from utils.tratamentoErros import mostrarErro
 
 def removerCpf(pagina, cpfLimpo):
-# Define um padrão de regex para encontrar CPF no texto da página    
-    padraoCpf = re.compile(r'CPF\s*n[°ºº.]*\s*([\d]{3}[-.\s]?[\d]{3}[-.\s]?[\d]{3}[-.\s]?\s?[\d]{2})')
+    try:
+    # Define um padrão de regex para encontrar CPF no texto da página    
+        padraoCpf = re.compile(r'CPF\s*n[°ºº.]*\s*([\d]{3}[-.\s]?[\d]{3}[-.\s]?[\d]{3}[-.\s]?\s?[\d]{2})')
 
-# Procura por todas as ocorrências do padrão no texto da página
-    cpfEncontrados = padraoCpf.findall(pagina.get_text())
+    # Procura por todas as ocorrências do padrão no texto da página
+        cpfEncontrados = padraoCpf.findall(pagina.get_text())
 
-    for encontrado in cpfEncontrados:
+        for encontrado in cpfEncontrados:
 
-# Remove caracteres não numéricos do CPF encontrado        
-        cpfPdf = re.sub(r'\D', '', encontrado)
+    # Remove caracteres não numéricos do CPF encontrado        
+            cpfPdf = re.sub(r'\D', '', encontrado)
 
-        if cpfPdf == cpfLimpo:
+            if cpfPdf == cpfLimpo:
 
-            retangulos = pagina.search_for(encontrado)
+                retangulos = pagina.search_for(encontrado)
 
-            for retangulo in retangulos:
-                anotacao = pagina.add_redact_annot(retangulo, fill=(0, 0, 0))
-                anotacao.update()
-                pagina.apply_redactions()
-            return True 
-
+                for retangulo in retangulos:
+                    anotacao = pagina.add_redact_annot(retangulo, fill=(0, 0, 0))
+                    anotacao.update()
+                    pagina.apply_redactions()
+                return True
+    except Exception as e:
+        mostrarErro(f"Ocorreu um erro inesperado: {e}")
     return False    
