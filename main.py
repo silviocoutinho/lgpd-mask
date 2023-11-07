271e65f9eb3594d4aefc1e9a30e969374b41cf4f
Nilson-Sj <nilsonjr180795@gmail.com>
2023-11-07 12:18:57 -0300

    Refatoracao: Extrai valores hard-code para variaveis no main.py

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from utils.imports import tk, gui, mostrarErro
+import os
+from dotenv import load_dotenv
+
+# Carrega as variáveis de ambiente do arquivo .env
+load_dotenv()
+
+# Define as variáveis para título e dimensão da aplicação a partir das variáveis de ambiente
+TITLE_APP = os.getenv("TITLE_APP")
+DIMENSION_APP = os.getenv("DIMENSION_APP")
 
 try:
     if __name__ == "__main__":
         app = tk.Tk()
-        app.title("Mascarar LGPD")
-        app.geometry("650x250")
+        app.title(TITLE_APP)
+        app.geometry(DIMENSION_APP)
 
         gui.create_gui(app)
 
