1ad270d6ef555263ba02d51da6f899ecc7b2c3ee
Nilson-Sj <nilsonjr180795@gmail.com>
2023-11-07 09:54:02 -0300

    Refatoracao: Adicionado tratamento de erros em main

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import tkinter as tk
 import interfaceGrafica.gui as gui
+from utils.tratamentoErros import mostrarErro
 
-if __name__ == "__main__":
-    app = tk.Tk()
-    app.title("Mascarar LGPD")
-    app.geometry("650x250")
+try:
+    if __name__ == "__main__":
+        app = tk.Tk()
+        app.title("Mascarar LGPD")
+        app.geometry("650x250")
 
-    gui.create_gui(app)
+        gui.create_gui(app)
 
-    app.mainloop()
+        app.mainloop()
+except Exception as e:
+    mostrarErro(f"Ocorreu um erro inesperado: {e}")
