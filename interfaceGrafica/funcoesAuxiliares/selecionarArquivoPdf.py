52512345e0dc836b4995acd85533642e62992b43
Nilson-Sj <nilsonjr180795@gmail.com>
2023-11-07 09:41:26 -0300

    Refatoracao: Adicionado tratamento de erros em selecionarArquivoPdf

--- interfaceGrafica/funcoesAuxiliares/selecionarArquivoPdf.py
+++ interfaceGrafica/funcoesAuxiliares/selecionarArquivoPdf.py
@@ -1,17 +1,18 @@
 import tkinter as tk
-from tkinter import filedialog, messagebox
+from tkinter import filedialog
+from utils.tratamentoErros import mostrarErro, mostrarAviso
 
 def selecionarArquivoPdf(app):
     try:
         app.caminhoArquivoPdf = filedialog.askopenfilename(filetypes=[("PDF Files", "*.pdf")])
         if app.caminhoArquivoPdf:
             if not app.caminhoArquivoPdf.lower().endswith(".pdf"):
-                messagebox.showerror("Erro", "Por favor, selecione um arquivo PDF válido.")
+                mostrarAviso("Por favor, selecione um arquivo PDF válido.")
             else:        
                 entradaArquivoPdf = app.children['!entry3']
                 entradaArquivoPdf.delete(0, tk.END)
                 entradaArquivoPdf.insert(0, app.caminhoArquivoPdf)
         else:
-            messagebox.showinfo("Aviso", "Nenhum arquivo selecionado.")
+            mostrarAviso("Nenhum arquivo selecionado.")
     except Exception as e:
-        messagebox.showerror("Erro", f"Ocorreu um erro durante a seleção do arquivo: {e}")
+        mostrarErro(f"Ocorreu um erro durante a seleção do arquivo: {e}")
