4dcfa85a65069259938dc43c24a2df2062b271d9
Nilson-Sj <nilsonjr180795@gmail.com>
2023-11-07 09:41:42 -0300

    Refatoracao: Adicionado tratamento de erros em mascararRg

--- mascararDados/mascararRg.py
+++ mascararDados/mascararRg.py
@@ -1,25 +1,28 @@
 import re
+from utils.tratamentoErros import mostrarErro
 
 def removerRg(pagina, rgLimpo):
-# Define um padrão de regex para encontrar RG no texto da página
-    padraoRG = re.compile(r'RG\s*n[°ºº.]*\s*((?:[A-Z]+\s*)?[-.\s]?\s*[\d.-]+(?:\s*[–-]\s*[A-Z/]+[A-Z/]+)?)')
+    try:
+    # Define um padrão de regex para encontrar RG no texto da página
+        padraoRG = re.compile(r'RG\s*n[°ºº.]*\s*((?:[A-Z]+\s*)?[-.\s]?\s*[\d.-]+(?:\s*[–-]\s*[A-Z/]+[A-Z/]+)?)')
 
-# Procura por todas as ocorrências do padrão no texto da página
-    rgEncontrados = padraoRG.findall(pagina.get_text())
+    # Procura por todas as ocorrências do padrão no texto da página
+        rgEncontrados = padraoRG.findall(pagina.get_text())
 
-    for encontrado in rgEncontrados:
+        for encontrado in rgEncontrados:
 
-# Remove caracteres não numéricos do RG encontrado
-        rgPdf = re.sub(r'\D', '', encontrado)
+    # Remove caracteres não numéricos do RG encontrado
+            rgPdf = re.sub(r'\D', '', encontrado)
 
-        if rgPdf == rgLimpo:
-            
-            retangulos = pagina.search_for(encontrado)
-            
-            for retangulo in retangulos:
-                anotacao = pagina.add_redact_annot(retangulo, fill=(0, 0, 0))
-                anotacao.update()
-                pagina.apply_redactions()
-            return True 
-
+            if rgPdf == rgLimpo:
+                
+                retangulos = pagina.search_for(encontrado)
+                
+                for retangulo in retangulos:
+                    anotacao = pagina.add_redact_annot(retangulo, fill=(0, 0, 0))
+                    anotacao.update()
+                    pagina.apply_redactions()
+                return True 
+    except Exception as e:
+        mostrarErro(f"Ocorreu um erro inesperado: {e}")
     return False    
