7adbf3391d4252bdacbb91588ead3dcb0258d0a7
Nilson-Sj <nilsonjr180795@gmail.com>
2023-11-08 10:41:23 -0300

    Refatoracao: Atualizei o selecionarArquivoPdf para usar mensagens dinamicas

--- interfaceGrafica/funcoesAuxiliares/selecionarArquivoPdf.py
+++ interfaceGrafica/funcoesAuxiliares/selecionarArquivoPdf.py
@@ -1,17 +1,17 @@
 from utils.imports import tk, filedialog
-from utils.tratamentoErros import mostrarErro, mostrarAviso
+from utils.tratamentoErros import mostrarArquivoInvalido, mostrarErroSelecaoArquivo, mostrarArquivoInexistente
 
 def selecionarArquivoPdf(app):
     try:
         app.caminhoArquivoPdf = filedialog.askopenfilename(filetypes=[("PDF Files", "*.pdf")])
         if app.caminhoArquivoPdf:
             if not app.caminhoArquivoPdf.lower().endswith(".pdf"):
-                mostrarAviso("Por favor, selecione um arquivo PDF válido.")
+                mostrarArquivoInvalido()
             else:        
                 entradaArquivoPdf = app.children['!entry3']
                 entradaArquivoPdf.delete(0, tk.END)
                 entradaArquivoPdf.insert(0, app.caminhoArquivoPdf)
         else:
-            mostrarAviso("Nenhum arquivo selecionado.")
+            mostrarArquivoInexistente()
     except Exception as e:
-        mostrarErro(f"Ocorreu um erro durante a seleção do arquivo: {e}")
+        mostrarErroSelecaoArquivo(e)
