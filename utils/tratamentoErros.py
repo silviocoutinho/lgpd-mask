1bc4dae0e956fcb6d9cf3c40a337347f89d66b4f
Nilson-Sj <nilsonjr180795@gmail.com>
2023-11-08 10:30:55 -0300

    Refatoracao: Atualizei o arquivo para usar mensagens dinamicas

--- utils/tratamentoErros.py
+++ utils/tratamentoErros.py
@@ -1,10 +1,26 @@
-from utils.imports import messagebox
+from utils.imports import tk
+from .mensagens import erroInesperado, erroSelecaoDestino, erroSelecaoArquivo, arquivoInvalido, arquivoNaoExiste, selecioneCampo, encontrado, naoEncontrado
 
-def mostrarErro(mensagem):
-    messagebox.showerror("Erro", mensagem)
+def mostrarErroInesperado(e):
+    tk.messagebox.showerror("Erro Inesperado", erroInesperado(e))
 
-def mostrarAviso(mensagem):
-    messagebox.showwarning("Aviso", mensagem)
+def mostrarErroSelecaoDestino(e):
+    tk.messagebox.showerror("Erro na Seleção do Destino", erroSelecaoDestino(e))
 
-def mostrarInfo(mensagem):
-    messagebox.showinfo("Informação", mensagem)
+def mostrarErroSelecaoArquivo(e):
+    tk.messagebox.showerror("Erro na Seleção do Arquivo", erroSelecaoArquivo(e))
+
+def mostrarArquivoInvalido():
+    tk.messagebox.showinfo("Arquivo Inválido", arquivoInvalido())
+
+def mostrarArquivoInexistente():
+    tk.messagebox.showinfo("Arquivo Inexistente", arquivoNaoExiste())
+
+def mostrarSelecaoCampo(campo):
+    tk.messagebox.showinfo("Seleção de Campo", selecioneCampo(campo))
+
+def mostrarEncontrado(campo):
+    tk.messagebox.showinfo("Encontrado", encontrado(campo))
+
+def mostrarNaoEncontrado(campo):
+    tk.messagebox.showinfo("Não Encontrado", naoEncontrado(campo))
