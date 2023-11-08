94c3ea2ec2f4ab61694ccd8af96a74b915d43b43
Nilson-Sj <nilsonjr180795@gmail.com>
2023-11-08 10:29:55 -0300

    Criacao: Adicionei arquivo de mensagens dinamicas

---
+++ utils/mensagens.py
@@ -0,0 +1,33 @@
+# Mensagem de sucesso
+def encontrado(campo):
+    return f"{campo} encontrado com sucesso."
+
+# Mensagem de não encontrado
+def naoEncontrado(campo):
+    return f"{campo} não foi encontrado."
+
+# Mensagens de erro durante a seleção da pasta de destino
+def erroSelecaoDestino(e):
+    return f"Ocorreu um erro durante a seleção da pasta de destino: {e}"
+
+# Mensagens de erro durante a seleção do arquivo
+def erroSelecaoArquivo(e):
+    return f"Ocorreu um erro durante a seleção do arquivo: {e}"
+
+
+# Mensagens gerais
+
+def selecioneCampo(campo):
+    return f"Por favor, selecione ao menos um {campo}."
+
+def arquivoNaoExiste():
+    return "O arquivo selecionado não existe."
+
+def erroProcessarPdf(e):
+    return f"Ocorreu um erro ao processar o arquivo PDF: {e}"
+
+def arquivoInvalido():
+    return "Por favor, selecione um arquivo PDF válido."
+
+def erroInesperado(e):
+    return f"Ocorreu um erro inesperado: {e}"
